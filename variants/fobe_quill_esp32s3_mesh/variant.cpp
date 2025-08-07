fa2514f68009e0848eecc921d0d593744e2c8d84
Chiho Sin <chihosin@icloud.com>
2025-08-07 17:01:15 +0800

    variants: update power initialization in variant.cpp Refactor the initVariant function to correctly set the OLED power state to LOW and add comments for clarity.
    Signed-off-by: ChihoSin chihosin@icloud.com

--- variants/fobe_quill_esp32s3_mesh/variant.cpp
+++ variants/fobe_quill_esp32s3_mesh/variant.cpp
@@ -4,10 +4,12 @@
 extern "C" {
 
 void initVariant(void) {
+  // Turn on the peripheral power
   pinMode(PIN_PERI_EN, OUTPUT);
   digitalWrite(PIN_PERI_EN, HIGH);
 
+  // Turn on the OLED power
   pinMode(PIN_OLED_EN, OUTPUT);
-  digitalWrite(PIN_OLED_EN, HIGH);
+  digitalWrite(PIN_OLED_EN, LOW);
 }
 }
